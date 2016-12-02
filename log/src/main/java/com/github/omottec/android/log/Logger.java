edd8a49c5e019f5370ff94090c46b686b3d2dcf7
omottec <omottec@gmail.com>
2016-12-02 17:43:42 +0800

    修改Logger的level逻辑

--- log/src/main/java/com/github/omottec/android/log/Logger.java
+++ log/src/main/java/com/github/omottec/android/log/Logger.java
@@ -3,165 +3,134 @@ package com.github.omottec.android.log;
 import android.util.Log;
 
 public final class Logger {
-    /**
-     * Priority constant for the println method;
-     */
-    public static final int VERBOSE = 2;
+
+    private static int sLevel = Log.VERBOSE;
+
+    private Logger() { throw new RuntimeException(); }
 
     /**
-     * Priority constant for the println method;
-     */
-    public static final int DEBUG = 3;
-
-    /**
-     * Priority constant for the println method;
-     */
-    public static final int INFO = 4;
-
-    /**
-     * Priority constant for the println method;
-     */
-    public static final int WARN = 5;
-
-    /**
-     * Priority constant for the println method;
-     */
-    public static final int ERROR = 6;
-
-    /**
-     * Priority constant for the println method.
-     */
-    public static final int ASSERT = 7;
-
-    public static final int NONE = 8;
-
-    private static int sLevel = VERBOSE;
-
-    private Logger() {}
-
-    /**
-     * Send a {@link #VERBOSE} log message.
+     * Send a VERBOSE log message.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      */
     public static int v(String tag, String msg) {
-        if (sLevel > VERBOSE) return 0;
+        if (sLevel > Log.VERBOSE) return 0;
         return Log.v(tag, msg);
     }
 
     /**
-     * Send a {@link #VERBOSE} log message and log the exception.
+     * Send a VERBOSE log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      * @param tr An exception to log
      */
     public static int v(String tag, String msg, Throwable tr) {
-        if (sLevel > VERBOSE) return 0;
+        if (sLevel > Log.VERBOSE) return 0;
         return Log.v(tag, msg, tr);
     }
 
     /**
-     * Send a {@link #DEBUG} log message.
+     * Send a DEBUG log message.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      */
     public static int d(String tag, String msg) {
-        if (sLevel > DEBUG) return 0;
+        if (sLevel > Log.DEBUG) return 0;
         return Log.d(tag, msg);
     }
 
     /**
-     * Send a {@link #DEBUG} log message and log the exception.
+     * Send a log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      * @param tr An exception to log
      */
     public static int d(String tag, String msg, Throwable tr) {
-        if (sLevel > DEBUG) return 0;
+        if (sLevel > Log.DEBUG) return 0;
         return Log.d(tag, msg, tr);
     }
 
     /**
-     * Send an {@link #INFO} log message.
+     * Send an INFO log message.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      */
     public static int i(String tag, String msg) {
-        if (sLevel > INFO) return 0;
+        if (sLevel > Log.INFO) return 0;
         return Log.i(tag, msg);
     }
 
     /**
-     * Send a {@link #INFO} log message and log the exception.
+     * Send a INFO log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      * @param tr An exception to log
      */
     public static int i(String tag, String msg, Throwable tr) {
-        if (sLevel > INFO) return 0;
+        if (sLevel > Log.INFO) return 0;
         return Log.i(tag, msg, tr);
     }
 
     /**
-     * Send a {@link #WARN} log message.
+     * Send a WARN log message.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      */
     public static int w(String tag, String msg) {
-        if (sLevel > WARN) return 0;
+        if (sLevel > Log.WARN) return 0;
         return Log.w(tag, msg);
     }
 
     /**
-     * Send a {@link #WARN} log message and log the exception.
+     * Send a WARN log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      * @param tr An exception to log
      */
     public static int w(String tag, String msg, Throwable tr) {
-        if (sLevel > WARN) return 0;
+        if (sLevel > Log.WARN) return 0;
         return Log.w(tag, msg, tr);
     }
 
     /*
-     * Send a {@link #WARN} log message and log the exception.
+     * Send a WARN log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param tr An exception to log
      */
     public static int w(String tag, Throwable tr) {
-        if (sLevel > WARN) return 0;
+        if (sLevel > Log.WARN) return 0;
         return Log.w(tag, tr);
     }
 
     /**
-     * Send an {@link #ERROR} log message.
+     * Send an ERROR log message.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      */
     public static int e(String tag, String msg) {
-        if (sLevel > ERROR) return 0;
+        if (sLevel > Log.ERROR) return 0;
         return Log.e(tag, msg);
     }
 
     /**
-     * Send a {@link #ERROR} log message and log the exception.
+     * Send a ERROR log message and log the exception.
      * @param tag Used to identify the source of a log message.  It usually identifies
      *        the class or activity where the log call occurs.
      * @param msg The message you would like logged.
      * @param tr An exception to log
      */
     public static int e(String tag, String msg, Throwable tr) {
-        if (sLevel > ERROR) return 0;
+        if (sLevel > Log.ERROR) return 0;
         return Log.e(tag, msg, tr);
     }
 
